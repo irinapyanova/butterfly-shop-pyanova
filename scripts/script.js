18f37b3fa7fa17453853b50a739b697368e8b656
Irina Pyanova <kizaru001@gmail.com>
2021-10-28 20:48:29 +0300

    Добавление скрипта, верстки страницы FAQ

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
-console.log('script work!');
+$(document).ready(function(){
+
+  $('.js-burger').on('click', function(){
+    $('.js-main-nav').toggleClass('main-nav-open');
+  })
+
+
+  let prevBtn;
+  $('.js-accordion-btn').on('click', function() {
+
+    if (prevBtn === $(this)[0]) {
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $('.js-accordion-btn').next().slideUp();
+    $(this).next().slideDown();
+    prevBtn = $(this)[0];
+  });
+
+});
